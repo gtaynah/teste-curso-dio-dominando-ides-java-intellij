0795094de264f8cb5ee6f0d594c4fb4942c10a16
Glecielle Silva <gtaynah@gmail.com>
2021-11-16 13:55:36 -0300

    `first commit`

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -6,6 +6,7 @@ public class PrimeiroPrograma {
     public static void main(String[] args) {
         Gato gato = new Gato();
         System.out.println(gato);
+        System.out.println(gato);
 
         /*int a = 5;
         int b = 3;
